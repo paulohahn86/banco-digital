31c77a3512bb9ee7887aa70c9c537e2f050bccb8
Paulo Augusto Hahn <paulo.hahn@hotmail.com>
2022-02-27 12:09:36 +0100

    #INTERFACE #HERANCA #POLIMORFISMO

---
+++ com/dio/IConta.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+public interface IConta {
+
+	void sacar(double valor);
+	
+	void depositar(double valor);
+	
+	void transferir(Conta contaDestino,double valor);
+
+	void imprimirExtrato();
+	
+}
